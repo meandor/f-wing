7860f064eb1e67ff547221072ab12ebe39bd45be
Daniel Schruhl <danielschruhl@gmail.com>
2019-02-03 10:39:44 +0100

    add logging

---
+++ src/Logger.ts
@@ -0,0 +1,38 @@
+import * as winston from 'winston';
+import { Logger } from 'winston';
+import { Config } from './Config';
+
+let loggerState: Logger;
+
+function initLogger() {
+    const fileFormat = winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.simple(),
+        winston.format.json()
+    );
+    loggerState = winston.createLogger({
+        level: Config.getInstance().get('log-level'),
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp(),
+            winston.format.printf((info: any) => {
+                return `${info.timestamp} [${info.level}]: ${info.message}`;
+            })
+        ),
+        defaultMeta: {service: Config.getInstance().get('app-name')},
+        transports: [
+            new winston.transports.File({filename: 'error.log', level: 'error', format: fileFormat}),
+            new winston.transports.File({filename: 'info.log', format: fileFormat}),
+            new winston.transports.Console()
+        ]
+    });
+}
+
+function getLogger() {
+    if (loggerState === undefined) {
+        initLogger();
+    }
+    return loggerState;
+}
+
+export const logger = getLogger();
