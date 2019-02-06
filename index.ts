002dbc512f93f6498ec296806ab1058e0ce48760
Daniel Schruhl <danielschruhl@gmail.com>
2019-02-06 17:54:13 +0100

    use registry when starting up app

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { Application } from './src/Application';
 import * as express from 'express';
+import { Registry } from 'prom-client';
 
 const expressInstance = express();
-const app = new Application(expressInstance);
+const metricsRegistry = new Registry();
+const app = new Application(expressInstance, metricsRegistry);
 app.start();
