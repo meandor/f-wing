b3a6c1a1aa5689fdc3f1dcf6b5f401946989f761
Daniel Schruhl <danielschruhl@gmail.com>
2019-02-07 16:15:39 +0100

    add export of default metrics registry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { Registry } from 'prom-client';
+
 export * from './Application';
 export * from './Config';
 export * from './logger';
+export const defaultMetricsRegistry = new Registry();
